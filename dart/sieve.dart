075df9eff781548d06238e55863b8b6eea523f75
stackcats <stackcats.hk@gmail.com>
2024-02-19 19:24:38 +0800

    dart/sieve.dart

---
+++ dart/sieve.dart
@@ -0,0 +1,24 @@
+class Sieve {
+  final int n;
+  Sieve(this.n);
+
+  get primes {
+    final lst = List.generate(n + 1, (_) => true, growable: false);
+    lst[0] = lst[1] = false;
+    for (int i = 2; i < lst.length; i++) {
+      if (!lst[i]) {
+        continue;
+      }
+      for (int k = i + i; k < lst.length; k += i) {
+        lst[k] = false;
+      }
+    }
+    final ans = [];
+    for (int i = 2; i < lst.length; i++) {
+      if (lst[i]) {
+        ans.add(i);
+      }
+    }
+    return ans;
+  }
+}
